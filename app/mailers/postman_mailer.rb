a4fe3d07e41d285a392b7216a00b52ef75d67a9f
TARUN NETHI <tarunteja.n@westagilelabs.in>
2021-09-29 16:06:56 +0530

    heroku changes

--- app/mailers/postman_mailer.rb
+++ app/mailers/postman_mailer.rb
@@ -1,6 +1,6 @@
 class PostmanMailer < ApplicationMailer
 	def purchased
-		mail(:to=>'taruntejanethi@gmail.com',:subject=>'your purchase is succussfully added')
+		mail(:to=>$text,:subject=>'your purchase is succussfully added')
 				
 	end
 end
